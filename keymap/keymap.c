8fb1ed3663822c873d560a31d704fd785c7f238d
Florian Brandner <florian@brandner.net>
2026-03-24 11:06:51 +0100

    Updated keymap

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -10,11 +10,11 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        TG(_BL),    _______,    KC_KB_VOLUME_DOWN, KC_KB_MUTE,   KC_KB_VOLUME_UP,
+        TG(_BL),    LWIN(KC_F1),LWIN(KC_F2),       LWIN(KC_F3),  LWIN(KC_F4),
         LWIN(KC_1), LWIN(KC_2), LWIN(KC_3),        LWIN(KC_4),   LWIN(KC_5), // META 1..5
-        KC_PGDN,    KC_HOME,    KC_PGUP,           KC_END,       KC_PGUP,
+        KC_PGDN,    KC_HOME,    KC_UP,             KC_END,       KC_PGUP,
         LSG(KC_A),  KC_LEFT,    KC_DOWN,           KC_RIGHT,     LSG(KC_S),
-        LWIN(KC_A), LWIN(KC_A), _______,           LWIN(KC_S),   LWIN(KC_S)
+        LWIN(KC_A), LWIN(KC_A), LWIN(KC_0),        LWIN(KC_S),   LWIN(KC_S)
    ),
     [_BL] = LAYOUT(
         _______,  _______,  _______, _______,  _______,
